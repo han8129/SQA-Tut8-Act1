6c182703850b35269e25ef6b76d5120cd3cf8bf7
han8129 <110092300+han8129@users.noreply.github.com>
2024-03-26 18:48:49 +0700

    Init Project

---
+++ src/main/java/vn/hanu/fit/sqa/group3/act1/Act1Application.java
@@ -0,0 +1,13 @@
+package vn.hanu.fit.sqa.group3.act1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Act1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Act1Application.class, args);
+	}
+
+}
---
+++ src/test/java/vn/hanu/fit/sqa/group3/act1/Act1ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hanu.fit.sqa.group3.act1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Act1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
